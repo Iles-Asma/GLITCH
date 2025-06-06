4ab56bf00fa7ea3b41429f878b8491e42de156c8
Iles-Asma <iles.a@icloud.com>
2025-06-06 23:07:20 +0200

    add glossaire css

--- src/slices/GlossaireItem/index.tsx
+++ src/slices/GlossaireItem/index.tsx
@@ -1,24 +1,26 @@
 import { FC } from "react";
 import { Content } from "@prismicio/client";
 import { SliceComponentProps, PrismicRichText } from "@prismicio/react";
+import styles from "./glossaire-item.module.css"; // import du CSS
 
-/**
- * Props for `SplitTitleDescription`.
- */
 export type SplitTitleDescriptionProps =
 	SliceComponentProps<Content.SplitTitleDescriptionSlice>;
 
-/**
- * Component for "SplitTitleDescription" Slices.
- */
 const SplitTitleDescription: FC<SplitTitleDescriptionProps> = ({ slice }) => {
 	return (
 		<section
 			data-slice-type={slice.slice_type}
 			data-slice-variation={slice.variation}
+			className={styles.split}
 		>
-			<PrismicRichText field={slice.primary.GlossaireItemTitle} />
-			<PrismicRichText field={slice.primary.GlossaireItemDescription} />
+			<div className={styles["split-left"]}>
+				<PrismicRichText field={slice.primary.GlossaireItemTitle} />
+			</div>
+			<div className={styles["split-right"]}>
+				<PrismicRichText
+					field={slice.primary.GlossaireItemDescription}
+				/>
+			</div>
 		</section>
 	);
 };
